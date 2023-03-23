4fcc161cdeddd39ab8c281f433be763baf866a68
MarianaMancan <80931170+MarianaMancan@users.noreply.github.com>
2023-03-22 22:31:08 -0300

    Add files via upload

---
+++ Cliente/src/cliente/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cliente;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
